c103fc68c4cf14f31c1d10f3c7e9f2e943d26e89
alex.paclin <alex.paclin@gmail.com>
2016-12-30 12:07:43 +0500

    Переопределение системы AspNet Identity

--- IdentityApp/Models/IdentityModels.cs
+++ IdentityApp/Models/IdentityModels.cs
@@ -9,7 +9,9 @@ namespace IdentityApp.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+		public int Age { get; set; } // добавляем свойство Age
+
+		public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
@@ -18,9 +20,19 @@ namespace IdentityApp.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	// добавляем модель Book
+	public class Book
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Author { get; set; }
+	}
+
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext()
+		public DbSet<Book> Books { get; set; }
+
+		public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
